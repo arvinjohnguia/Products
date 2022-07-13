fa22960a68dce907a5175e31e835abf800758e56
Andrea <107744988+andreajesse@users.noreply.github.com>
2022-07-13 20:18:03 +0800

    FIXED PRODUCT FORM AND SHOP UI

--- base/models.py
+++ base/models.py
@@ -12,9 +12,16 @@ class Category(models.Model):
     def __str__(self):
         return self.Cat_Name
 
+#class ProductType(models.Model):
+#    ProdType_Name = models.CharField(max_length=200, verbose_name="Product Type")
+
+#    def __str__(self):
+#        return self.ProdType_Name
+
 class Product(models.Model):
     insProd_Name = models.CharField(max_length=200, verbose_name="Product Name")
     Cat_Name = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True, verbose_name="Category")
+    #ProdType_Name = models.ForeignKey(ProductType, on_delete=models.SET_NULL, null=True, blank=True, verbose_name="Category")
     insProd_Desc = models.TextField(max_length=200, null=True, blank=True, verbose_name="Description")
     insProd_stockQty = models.IntegerField(verbose_name="Stock Quantity")
     insProd_Price = models.FloatField(verbose_name="Price")
